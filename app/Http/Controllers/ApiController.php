386feb629ff442e4c15e043bdf8832bf832b677e
x-bal <mxbal026@gmail.com>
2023-08-18 09:27:29 +0700

    api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Device;
+use App\Models\Karyawan;
+use App\Models\Log;
+use App\Models\Setting;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ApiController extends Controller
+{
+    public function store(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $now = Carbon::now('Asia/Jakarta')->format('YmdHis');
+
+            $foto = $request->file('foto');
+            $fotoUrl = $foto->storeAs('logs', $now . '-' . rand(1000, 9999) . '.' . $foto->extension());
+
+            $device = Device::where('iddev', $request->id_device)->first();
+
+            $user = Karyawan::where('employee_id', $request->employeeid)->first();
+
+            $setting = Setting::find(1);
+
+            if ($request->beard == 0 && $request->moustache == 0 && $request->suhu < $setting->limit) {
+                $status = "Healthy";
+            } else {
+                $status = "Not Healthy";
+            }
+
+            Log::create([
+                'user_id' => $user->user_id,
+                'device_id' => $device->id,
+                'moustache' => $request->moustache,
+                'beard' => $request->beard,
+                'suhu' => $request->suhu,
+                'waktu' => $request->waktu,
+                'foto' => $fotoUrl,
+                'status' => $status
+            ]);
+
+            DB::commit();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => "Log berhasil disimpan"
+            ]);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage()
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\TicketController;
 use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/store', [ApiController::class, 'store']);
