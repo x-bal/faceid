edf3f98809aae6f8c6b6932901eb394824c4cfb5
Muhamad iqbal Ilham Rahayu <70595626+x-bal@users.noreply.github.com>
2023-08-25 13:45:14 +0700

    feat(middleware) : middleware dashboard

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
-});
+})->middleware('guest');
 
 Route::post('/login', LoginController::class)->name('login');
 
